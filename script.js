9f050e687209e723600e8cefc70e149db5c32b64
imanimonae1 <imsappington@cps.edu>
2025-05-22 15:37:06 +0000

    9

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let clearButton = document.querySelector(".clear");
 let equalsButton = document.querySelector(".equals");
 let result = document.querySelector(".result");
 let doggieAge = document.querySelector(".doggieAge");
-
+let golden2=document.querySelector(".golden2");
+golden2.style.display="none";
+let golden1= document.querySelector(".golden1");
 const calculateDoggieAge = (humanAge) => {
     let doggieAge = humanAge * 7;
   result.innerHTML =
@@ -16,9 +18,13 @@ equalsButton.addEventListener("click", () => {
 let x= Number(input.value);
   console.log(x)
   calculateDoggieAge(x);
+  golden2.style.display="block";
+  golden1.style.display="none";
 });
 
 clearButton.onclick = function () {
   input.value = "";
   result.innerHTML = "";
+  golden1.style.display="block";
+  golden2.style.display="none";
 };
